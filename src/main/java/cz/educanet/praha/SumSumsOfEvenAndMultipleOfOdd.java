a1adf7631614b02c0aa24fbff1636128996c0ab4
Martin Macura <mr.martin.macura@gmail.com>
2021-03-28 17:35:21 +0200

    added new tasks

---
+++ src/main/java/cz/educanet/praha/SumSumsOfEvenAndMultipleOfOdd.java
@@ -0,0 +1,16 @@
+package cz.educanet.praha;
+
+import java.util.Scanner;
+
+public class SumSumsOfEvenAndMultipleOfOdd {
+
+    public static int sumSumsOfEvenAndMultipleOfOdd(Scanner scanner) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number separated by a new line");
+        System.out.println(sumSumsOfEvenAndMultipleOfOdd(sc));
+    }
+}
