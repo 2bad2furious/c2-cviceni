3b1fcb0bc64956c9b32893824afcd93ab55e68e8
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-03-29 20:52:24 +0200

    Udelal jsem Square

--- src/main/java/cz/educanet/praha/PrintedSquare.java
+++ src/main/java/cz/educanet/praha/PrintedSquare.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class PrintedSquare {
     public static String printSquare(int side) {
+
+        int i, j;
+
+        for (i = 1; i <= side; i++)
+        {
+            for (j = 1; j <= side; j++)
+            {
+                if (i == 1 || i == side ||
+                        j == 1 || j == side)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
         return "";
     }
 
