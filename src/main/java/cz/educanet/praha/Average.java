26186b46c6634378d6e4853b2a24cc8162aa85bf
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-04-04 17:04:20 +0200

    mam

--- src/main/java/cz/educanet/praha/Average.java
+++ src/main/java/cz/educanet/praha/Average.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Average {
 
     public static double average(Scanner scanner) {
+
         return 1;
     }
 
     public static void main(String[] args) {
+
         System.out.println(average(new Scanner(System.in)));
     }
 }
--- src/main/java/cz/educanet/praha/Cube.java
+++ src/main/java/cz/educanet/praha/Cube.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Cube {
     public static int sumOfAreasAndVolumes(Scanner scanner) {
+
         return 0;
     }
 
     public static void main(String[] args) {
+
         System.out.println(sumOfAreasAndVolumes(new Scanner(System.in)));
     }
 }
