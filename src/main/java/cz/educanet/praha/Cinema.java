88bacb1509599a362bc631e8d56d4d3c21d38af5
Martin Macura <mr.martin.macura@gmail.com>
2021-04-12 14:16:40 +0200

    Cinema

---
+++ src/main/java/cz/educanet/praha/Cinema.java
@@ -0,0 +1,10 @@
+package cz.educanet.praha;
+
+import java.util.Scanner;
+
+public class Cinema {
+
+    public static int freeSpaces(String[] namesOnSeat, Scanner sc) {
+        return 0;
+    }
+}
