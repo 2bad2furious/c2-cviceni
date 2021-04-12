2ccfde906f7197fbec15fb6c390ff120f99929c4
Martin Macura <mr.martin.macura@gmail.com>
2021-04-12 14:49:28 +0200

    Cinema main

--- src/main/java/cz/educanet/praha/Cinema.java
+++ src/main/java/cz/educanet/praha/Cinema.java
@@ -7,4 +7,9 @@ public class Cinema {
     public static int freeSpaces(String[] namesOnSeat, Scanner sc) {
         return 0;
     }
+
+    public static void main(String[] args) {
+        int freeSpaces = freeSpaces(new String[]{null, null, "2bad2furious", null, null, "monarezio"}, new Scanner(System.in));
+        System.out.println("Free spaces: " + freeSpaces);
+    }
 }
