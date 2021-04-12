f1bcb71e5237b69ff5dd64f380c5efb707d839f1
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-04-12 13:54:41 +0200

    mam

--- src/main/java/cz/educanet/praha/Cube.java
+++ src/main/java/cz/educanet/praha/Cube.java
@@ -4,12 +4,19 @@ import java.util.Scanner;
 
 public class Cube {
     public static int sumOfAreasAndVolumes(Scanner scanner) {
+        int volume = 0;
+        int surface = 0;
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();
+            volume = volume+n*n*n;
+            surface =surface+6*n*n;
 
-        return 0;
+        }
+
+        return volume + surface;
     }
 
     public static void main(String[] args) {
-
         System.out.println(sumOfAreasAndVolumes(new Scanner(System.in)));
     }
 }
