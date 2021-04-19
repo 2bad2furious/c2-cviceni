df19d7211ac0f64b472c334ab4f81757b40b1c42
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-04-19 13:22:41 +0200

    Update Average.java

--- src/main/java/cz/educanet/praha/Average.java
+++ src/main/java/cz/educanet/praha/Average.java
@@ -5,12 +5,20 @@ import java.util.Scanner;
 public class Average {
 
     public static double average(Scanner scanner) {
+        double sum = 0;
+        double average = 0;
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            for (int i = 0; i < n; i++) {
+              sum += n;
+              average = average + sum/n;
+              }
+            }
 
-        return 1;
+        return average;
     }
 
     public static void main(String[] args) {
-
         System.out.println(average(new Scanner(System.in)));
     }
 }
