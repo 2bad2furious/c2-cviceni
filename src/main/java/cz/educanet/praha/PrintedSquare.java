83ae398f055476782574a02918d1dce1736ace23
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-04-27 14:39:40 +0200

    Update PrintedSquare.java

--- src/main/java/cz/educanet/praha/PrintedSquare.java
+++ src/main/java/cz/educanet/praha/PrintedSquare.java
@@ -3,19 +3,8 @@ package cz.educanet.praha;
 import java.util.Scanner;
 
 public class PrintedSquare {
-    public static String printSquare(int side) {
-        int i,j;
-        String b ="";
-        for(i = 1; i <= side; i++){
-            for(j = 1; j <= side; j++){
-                if (i == 1 || i == side || j == 1 || j == side)
-                    b = b+("*");
-                else
-                    b = b+(" ");
-            }
-            b = b+"\n";
-        }
-        return b;
+    public static String printSquare(int side) { 
+        return 0;
     }
 
     public static void main(String[] args) {
